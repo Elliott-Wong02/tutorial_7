a6185eeeb0dc2425ce7cf6eff81aa34cf277297c
Elliott-Wong02 <ekw120@ic.ac.uk>
2022-11-22 17:16:34 +0000

    1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
         for (int i=1; i<=10; i=i+1){
             System.out.println(i);
         }
+        int sum = 0;
+        for (int i=30; i<=40; i=i+1){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
